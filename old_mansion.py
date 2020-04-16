82a1e547641e886b759ee21dfc2b8244b26d811d
Andrew <franklin.andrew231@gmail.com>
2020-04-16 03:12:32 +0000

    Add to story

--- old_mansion.py
+++ old_mansion.py
@@ -15,8 +15,8 @@ parlor:
 You enter a small parlor. A sofa, chairs and small armtables fill
 the space. A TV hangs on the wall with an old VHS cassette of "Hawaii 5 0"
 on the vcr below it. A door stands open at the far end.
-continue, back
-start, kitchen
+back
+start
 
 2nd:
 There is a huge dining table with a chandelier with blue candles the size of
@@ -72,13 +72,64 @@ globe
 hawaii:
 As you touch the island hawaii a big X appears over it. Now it all makes sense!
 you say. Its a treasure map!
-chpter 2 ,start
-chpter 2,start
+go on to chapter 2
+chapter2
 
-chpter 2:
+chapter2:
+Welcome to chapter 2 One and all! thank you so much for playing this game!
+After this chapter there will be One more! And in case your wondering,NO
+SPOILERS!
+go to the chapter2 start
+chapter2 start
 
+chapter2 start:
+You go out of the mansion. The stranger says "I knew you would find the
+treasure map! I will accompany you the rest of the way. Now lets go to the
+forest!
+trust him and go into the forest, don't trust him and go without him
+forest with him, forest without him
 
+forest without him:
+we'll i am very sorry but i WILL HAVE TO KILL YOU SINCE YOU DONT TRUST ME!
+you died.
+back
+chapter2 start
+
+forest with him:
+You go into the forest.  Ahead you see a sign it says go left to the docks and
+go right to the city.The city slopes up. the docks slope down.
+left, right
+docks,city
+
+city:
+uh oh, ROBBERS! stick em up the robbers said. oh no you lost the map
+back
+chapter2 start
+
+Docks:
+The stranger says here i have enough money to buy a STEAMSHIP WITH CANNONS AND
+AN ARMY IN IT!nice you say lets get to sea!You soon sail out.You see pirates to
+the left blocking the way.
+go around them, fight them
+fight, RUN!
+
+RUN!:
+You try to go around them.They just fire cannons at you and you sink you died!
+back
+docks
+
+fight:
+You fought. You got captured. The pirates have taken your treasure map and you
+will be FORCED TO WALK THE PLANK!
+escape the ship, surrender and walk the plank
+escape,plank
+
+plank:
+You and stranger walked the plank and eaten by sharks. YOU DIED
+back,
+fight
 """
+
 import gamelib
 
 if __name__ == "__main__":
