b37f9abff91acff161e53910832d62f6f25cf829
Andrew <franklin.andrew231@gmail.com>
2020-04-19 23:49:13 +0000

    Add levels

--- level1.py
+++ level1.py
@@ -1,4 +1,16 @@
-meteors = [
+meteor1 = [
+  {'size': 6, 'delay': 0, 'x': .30, 'y': .2, 'speed': -5},
   {'size': 4, 'delay': 10, 'x': .5, 'y': 1, 'speed': 1},
-  {'size': 3, 'delay': 0, 'x': .25, 'y': .2, 'speed': -2},
-]
+  {'size': 6, 'delay': 0, 'x': .75, 'y': .2, 'speed': -2},
+  {'size': 4, 'delay': 10, 'x': .15, 'y': 4, 'speed': -5},
+  ]
+
+meteor2 = [
+  {'size': 4, 'delay': 0, 'x': .10, 'y': .10, 'speed': -4},
+  {'size': 6, 'delay': 5, 'x': .35, 'y': .35, 'speed': -3},
+  {'size': 5, 'delay': 0, 'x': .60, 'y': .60, 'speed': -4},
+  {'size': 6, 'delay': 0, 'x': .75, 'y': .75, 'speed': -2},
+  {'size': 3, 'delay': 0, 'x': .99, 'y': .99, 'speed': -6},
+  ]
+
+meteors = [meteor1, meteor2]
